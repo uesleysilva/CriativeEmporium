2c8fa67705e50ca2f31523b665ac599efff847aa
uesleysilva <uesleyk1@hotmail.com>
2023-09-28 23:27:22 -0300

    newatt

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+const handleClick = () => {
+  const element = document.querySelector(".menuitem");
+  if (element.style.) {
+    console.log("aaa");
+    element.style.display = "none";
+  } else {
+    element.style.display = "column";
+  }
+};
